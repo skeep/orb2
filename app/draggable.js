85e349e9334f415db6fbcf85388fa5dd5ee5df1a
Suman Paul <sumank.paul@gmail.com>
2014-10-03 22:53:57 +0530

    update link

--- app/draggable.js
+++ app/draggable.js
@@ -1,30 +1,38 @@
 angular.module('orb').directive('draggable', function (Screen, Link) {
 
+  var draggableConfig = {};
+
+  draggableConfig.scroll = true;
+
+
+  function postLink(scope, element) {
+
+    draggableConfig.stop = function stopDragging(e, ui) {
+      if (scope.draggableType === 'screen') {
+        Screen.update(scope.draggableId, {
+          left: ui.position.left,
+          top: ui.position.top
+        });
+      } else if (scope.draggableType === 'link') {
+        Link.update(scope.draggableId, {
+          left: ui.position.left,
+          top: ui.position.top
+        });
+      }
+    };
+
+    if (scope.draggableType === 'link') {
+      draggableConfig.containment = 'parent';
+    }
+    $(element).draggable(draggableConfig);
+  }
+
   return {
     scope: {
       draggableId: '=',
       draggableType: '@'
     },
-    link: function postLink(scope, element) {
-      $(element).draggable({
-        stop: function stopDragging(e, ui) {
-          if (scope.draggableType === 'screen') {
-            Screen.update(scope.draggableId, {
-              left: ui.offset.left,
-              top: ui.offset.top
-            });
-          } else if (scope.draggableType === 'link') {
-            Link.update(scope.draggableId, {
-              left: ui.offset.left,
-              top: ui.offset.top
-            });
-          }
-
-        },
-        scroll: true
-      });
-    }
+    link: postLink
   };
-
 
 });
